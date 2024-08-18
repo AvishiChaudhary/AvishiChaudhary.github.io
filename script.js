3c96101b5518da3ef59d0a3d80574b8f241e52b6
AvishiChaudhary <ac2486@njit.edu>
2024-08-17 20:08:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Initial display
     showQuote(currentQuoteIndex);
 
-    // Event listeners for buttons
+    // Event listeners for arrows
     document.querySelector('.next').addEventListener('click', showNextQuote);
     document.querySelector('.prev').addEventListener('click', showPrevQuote);
 });
@@ -29,27 +29,16 @@ document.addEventListener('DOMContentLoaded', function() {
 // Toggle menu visibility
 document.querySelector('.menu-icon').addEventListener('click', function() {
     var menu = document.getElementById('menu');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 });
 
 // Scroll to top functionality
 const scrollToTopButton = document.querySelector('.scroll-to-top');
 
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 300) { // Show button after scrolling down 300px
-        scrollToTopButton.style.display = 'block';
-    } else {
-        scrollToTopButton.style.display = 'none';
-    }
+    scrollToTopButton.style.display = window.scrollY > 300 ? 'block' : 'none';
 });
 
 scrollToTopButton.addEventListener('click', function() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
